45671d48695c82faf08dbbcd71fe2fb7feaede3d
Nikolay Ninov <nikolaitn@gmail.com>
2017-06-15 22:31:56 +0300

    Add git ignore

---
+++ src/main/java/com/nninov/FullstackIntellijApplication.java
@@ -0,0 +1,12 @@
+package com.nninov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackIntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackIntellijApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nninov/FullstackIntellijApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nninov;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FullstackIntellijApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
