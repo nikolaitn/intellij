e2e5148bced9e7b1e726e8dd87d94a540850abff
Nikolay Ninov <nninov@axway.com>
2017-06-22 14:16:43 +0300

    Added i18n capabilities

--- src/test/java/com/nninov/FullstackIntellijApplicationTests.java
+++ src/test/java/com/nninov/FullstackIntellijApplicationTests.java
@@ -1,16 +1,26 @@
 package com.nninov;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.devopsbuddy.web.i18n.I18NService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FullstackIntellijApplicationTests {
 
+	@Autowired
+	private I18NService i18NService;
+	
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocalService() {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected Strings don't match.", expectedResult, actual);
 	}
-
 }
